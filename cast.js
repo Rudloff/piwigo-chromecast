043261163b220ba4f6ca02066d03a1aaeb268b8a
Pierre Rudloff <contact@rudloff.pro>
2014-04-04 23:09:04 +0200

    Initial commit

---
+++ cast.js
@@ -0,0 +1,101 @@
+/*global chrome*/
+/*jslint devel: true, browser: true */
+var castBtn;
+var session, currentMedia;
+
+function receiverListener() {
+    'use strict';
+    console.log('receiverListener');
+}
+
+function onMediaDiscovered(how, media) {
+    'use strict';
+    currentMedia = media;
+    castBtn.classList.add('cast-btn-on');
+}
+
+function onStopCast() {
+    'use strict';
+    castBtn.classList.remove('cast-btn-on');
+    session = undefined;
+}
+
+function stopCast() {
+    'use strict';
+    session.stop(onStopCast);
+}
+
+function onMediaError() {
+    'use strict';
+    console.log('onMediaError');
+    stopCast();
+}
+
+function castMedia() {
+    'use strict';
+    var imgURL = 'https://rudloff.pro/tests/piwigo/action.php?id=' + window.location.search.substr(2, 1) + '&part=e&download', mediaInfo = new chrome.cast.media.MediaInfo(imgURL, 'image/jpeg'), request = new chrome.cast.media.LoadRequest(mediaInfo);
+    console.log('Casting', imgURL);
+    session.loadMedia(request, onMediaDiscovered.bind(this, 'loadMedia'), onMediaError);
+}
+
+function sessionListener(e) {
+    'use strict';
+    if (castBtn) {
+        session = e;
+        session.addMediaListener(onMediaDiscovered.bind(this, 'addMediaListener'));
+        if (session.media.length !== 0) {
+            castMedia();
+            onMediaDiscovered('onRequestSessionSuccess', session.media[0]);
+        }
+    }
+}
+
+function onRequestSessionSuccess(e) {
+    'use strict';
+    session = e;
+    castMedia();
+}
+
+function onLaunchError() {
+    'use strict';
+    console.log('onLaunchError');
+}
+
+function toggleCast() {
+    'use strict';
+    if (session) {
+        stopCast();
+    } else {
+        chrome.cast.requestSession(onRequestSessionSuccess, onLaunchError);
+    }
+}
+
+function onInitSuccess() {
+    'use strict';
+    castBtn = document.getElementById('cast_btn_launch');
+    if (castBtn) {
+        castBtn.addEventListener('click', toggleCast, false);
+    }
+}
+
+function onError() {
+    'use strict';
+    console.log('onError');
+}
+
+function initializeCastApi() {
+    'use strict';
+    var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID), apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
+    chrome.cast.initialize(apiConfig, onInitSuccess, onError);
+}
+
+function loadCastApi(loaded, errorInfo) {
+    'use strict';
+    if (loaded) {
+        initializeCastApi();
+    } else {
+        console.log(errorInfo);
+    }
+}
+
+window['__onGCastApiAvailable'] = loadCastApi;
---
+++ main.inc.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Version: 0.1
+Plugin Name: ChromeCast
+Author: Pierre Rudloff <contact@rudloff.pro>
+Description: Cast your pictures to a ChromeCast
+*/
+
+if (!defined('PHPWG_ROOT_PATH')) die('Hacking attempt!');
+ 
+add_event_handler('loc_begin_page_tail', 'cast_add_api');
+function cast_add_api()
+{
+  global $page, $template;
+  $template->append('footer_elements', '
+    <script type="text/javascript" src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js"></script>
+    <script src="plugins/chromecast/cast.js"></script>
+  ');
+}
+add_event_handler('loc_begin_page_header', 'cast_add_btn');
+function cast_add_btn()
+{
+    global $template;
+    $template->add_picture_button('
+    <a id="cast_btn_launch" rel="nofollow" class="pwg-state-default pwg-button" title="Cast this picture"> <span class="pwg-icon pwg-icon-cast"></span><span class="pwg-button-text">Cast</span></a>
+    ', EVENT_HANDLER_PRIORITY_NEUTRAL);
+    $template->append('head_elements', '
+        <link rel="stylesheet" href="plugins/chromecast/style.css" />
+    ');
+}
+
+?>
